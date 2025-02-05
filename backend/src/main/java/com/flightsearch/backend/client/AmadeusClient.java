58e72a331eee964cdca4e37f4bc8e2daba02de98
ramonGomezEnc <ramon.gomez@encora.com>
2025-02-04 22:14:40 -0700

    feat: refresh token

--- backend/src/main/java/com/flightsearch/backend/client/AmadeusClient.java
+++ backend/src/main/java/com/flightsearch/backend/client/AmadeusClient.java
@@ -7,26 +7,33 @@ import com.flightsearch.backend.model.Airport;
 import com.flightsearch.backend.model.flightoptions.GeneralResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.w3c.dom.Node;
 
 @Component
 public class AmadeusClient {
 
     private final RestTemplate restTemplate;
     private final String baseUrl;
-    private final String clientKey;
+    private final String clientSecret;
+    private String clientKey;
     private final ObjectMapper objectMapper;
 
     public AmadeusClient(
             RestTemplate restTemplate,
             @Value("${api.base_url}") String baseUrl,
-            @Value("${api.client_key}") String clientKey
+            @Value("${api.client_key}") String clientKey,
+            @Value("${api.client_secret}") String clientSecret
     ) {
         this.restTemplate = restTemplate;
         this.baseUrl = baseUrl;
         this.clientKey = clientKey;
+        this.clientSecret = clientSecret;
         this.objectMapper = new ObjectMapper();
     }
 
@@ -34,6 +41,33 @@ public class AmadeusClient {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(clientKey);
         return new HttpEntity<>(headers);
+    }
+
+    private synchronized void refreshToken() {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+            MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+            body.add("grant_type", "client_credentials");
+            body.add("client_id", clientKey);
+            body.add("client_secret", clientSecret);
+
+            HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+            System.out.println(clientKey);
+            System.out.println(clientSecret);
+            System.out.println(baseUrl);
+            ResponseEntity<JsonNode> response = restTemplate.postForEntity(baseUrl + "/v1/security/oauth2/token", request, JsonNode.class);
+
+
+            if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
+                throw new RuntimeException("Error when calling Amadeus API: " + response.getStatusCode());
+            }
+
+            this.clientKey = response.getBody().path("access_token").asText();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public Airport fetchAirport(String airportKeyword) {
@@ -46,7 +80,14 @@ public class AmadeusClient {
                 .queryParam("view", "LIGHT");
 
         String uri = uriBuilder.toUriString();
-        ResponseEntity<JsonNode> response = restTemplate.exchange(uri, HttpMethod.GET, entity, JsonNode.class);
+        ResponseEntity<JsonNode> response = null;
+
+        try {
+            response = restTemplate.exchange(uri, HttpMethod.GET, entity, JsonNode.class);
+        } catch (HttpClientErrorException.Unauthorized e) {
+            refreshToken();
+            return fetchAirport(airportKeyword);
+        }
 
         if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
             throw new RuntimeException("Error when calling Amadeus API: " + response.getStatusCode());
@@ -89,7 +130,21 @@ public class AmadeusClient {
         }
 
         String uri = uriBuilder.toUriString();
-        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = null;
+        try {
+            response = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        } catch (HttpClientErrorException.Unauthorized e) {
+            refreshToken();
+            return fetchFlightData(
+                    departureAirportCode,
+                    arrivalAirportCode,
+                    departureDate,
+                    arrivalDate,
+                    numAdults,
+                    currency,
+                    nonStop
+            );
+        }
 
         if (!response.getStatusCode().is2xxSuccessful() || response.getBody() == null) {
             throw new RuntimeException("Error when calling Amadeus API: " + response.getStatusCode());
