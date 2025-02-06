fa7403791947af6f2c2b8808b35a1961e535e3d6
ramonGomezEnc <ramon.gomez@encora.com>
2025-02-05 19:47:18 -0700

    feat: landing page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-400 to-blue-600">
+      <h1 className="text-white text-4xl font-bold mb-8">
+        fligh-search.io
+      </h1>
+    </div>
+  );
+}
+
+export default App;
